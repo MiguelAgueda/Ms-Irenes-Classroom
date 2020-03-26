8420e10c45d0f9eb14c57d7d03ac477a696f5807
Miguel Agueda <40192229+MiguelAgueda@users.noreply.github.com>
2020-03-26 10:15:06 -0700

    Updated ProductionGZip for Gcloud

--- config/index.js
+++ config/index.js
@@ -40,7 +40,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, 'dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: './',
+    assetsPublicPath: '/',
 
     host: process.env.HOST, // can be overwritten by process.env.HOST
     port: process.env.PORT, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
@@ -57,7 +57,7 @@ module.exports = {
     // Surge or Netlify already gzip all static assets for you.
     // Before setting to `true`, make sure to:
     // npm install --save-dev compression-webpack-plugin
-    productionGzip: false,
+    productionGzip: true,
     productionGzipExtensions: ['js', 'css'],
 
     // Run the build command with an extra argument to
